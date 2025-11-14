bb55bfb15f04aa62daa3fc85226752c67e9ae1e1
Vivian Santana <santanavivian245@gmail.com>
2025-11-13 23:40:41 -0300

    carrossel mobile

--- slide.js
+++ slide.js
@@ -1,15 +1,31 @@
-// Slide automático da galeria de projetos
+// Slides automáticos da galeria de projetos
 let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval( function(){
-    nextImage();
+    nextImageDesktop();
 }, 5000)
 
-function nextImage(){
+function nextImageDesktop(){
     count++;
     if(count>10){
         count = 1;
     }
     document.getElementById("radio"+count).checked = true;
+}
+
+// Slide automático da galeria mobile
+let conta = 1;
+document.getElementById("radio-mob1").checked = true;
+
+setInterval(function() {
+    nextImageMobile();
+}, 5000);
+
+function nextImageMobile() {
+    conta++;
+    if (conta > 9) {   //APENAS 9 radios
+        conta = 1;
+    }
+    document.getElementById("radio-mob" + conta).checked = true;
 }
